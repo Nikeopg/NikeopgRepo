ba14de62881169cf4efb77cde46a3460c0052aff
Nikeopg <vns95@bk.ru>
2023-02-13 22:44:30 +0300

    NEW_FILE

--- Stepik Lessons/ex 3.7.py
+++ Stepik Lessons/ex 3.7.py
@@ -1,6 +1,5 @@
 games_amount = int(input())
 team = []
-
 table = []
 teams = {}
 points, match_amount, wp, draw, gg = 0, 0, 0, 0, 0
@@ -12,13 +11,35 @@ while cnter < games_amount:
     table.append(match.split(";"))
     cnter += 1
 
-print(table)
-
 for i in range(len(table)):
-    teams[table[i][0]] = [match_amount,wp, draw, gg, points]
+    teams[table[i][0]] = [match_amount, wp, draw, gg, points]
     teams[table[i][2]] = [match_amount, wp, draw, gg, points]
 
-print(teams)
+for i in range(len((table))):
+    table[i][1] = int(table[i][1])
+    table[i][3] = int(table[i][3])
+    if table[i][1] > table[i][3]:
+        teams[table[i][0]][0] += 1
+        teams[table[i][0]][1] += 1
+        teams[table[i][0]][4] += 3
+        teams[table[i][2]][0] += 1
+        teams[table[i][2]][3] += 1
+    elif table[i][1] < table[i][3]:
+        teams[table[i][2]][0] += 1
+        teams[table[i][2]][1] += 1
+        teams[table[i][2]][4] += 3
+        teams[table[i][0]][0] += 1
+        teams[table[i][0]][3] += 1
+    else:
+        teams[table[i][0]][0] += 1
+        teams[table[i][0]][2] += 1
+        teams[table[i][0]][4] += 1
+        teams[table[i][2]][0] += 1
+        teams[table[i][2]][2] += 1
+        teams[table[i][2]][4] += 3
+
+for club, res in teams.items():
+    print(club, *res)
 
 """ Напишите программу, которая принимает на стандартный вход список игр футбольных команд
 с результатом матча и выводит на стандартный вывод сводную таблицу результатов всех матчей.
