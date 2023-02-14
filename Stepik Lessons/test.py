3bd35b1f10a85d6e62530569340f0284a6fac387
Nikeopg <vns95@bk.ru>
2023-02-14 13:53:57 +0300

    NEW_FILE

--- Stepik Lessons/test.py
+++ Stepik Lessons/test.py
@@ -1,10 +1,3 @@
-# dic = {input().lower() for i in range(int(input()))}
-# print(dic, type(dic))
-#
-# x = set(input())
-# print(x)
+x = 15.222222
 
-wrd = set()
-for w in range(int(input())):
-    wrd |= {i.lower() for i in input().split()}
-print(wrd)
+print(round(x,5))
